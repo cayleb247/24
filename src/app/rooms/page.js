8b55ff27a9060b412d7442465df32c89931024cf
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-07-17 20:17:25 -0400

    Make /rooms page and rooms list

--- src/app/rooms/page.js
+++ src/app/rooms/page.js
@@ -1,17 +1,27 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./rooms.module.css";
+import RoomsList from "@/components/RoomsList";
 
 export default function Rooms() {
+  const [search, setSearch] = useState(null);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Find a Room</h1>
 
       <div className={styles.roomsContainer}>
-        <form action="">
-          <input type="text" placeholder="search rooms" />
-        </form>
-        <div className={styles.roomsList}></div>
+        <input
+          type="text"
+          placeholder="search rooms"
+          name="search"
+          onChange={(e) => {
+            setSearch(e.target.value)
+            console.log(search);
+        }
+        }
+        />
+        <RoomsList></RoomsList>
       </div>
     </div>
   );
--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,12 +1,12 @@
-
-
-
+import styles from "./RoomsList.module.css"
+import Room from "./Room.js"
+// import rooms from "../../server.js"
 
 export default function RoomsList({ search }) {
 
   return (
     <div className={styles.roomsListContainer}>
-        
+
     </div>
   );
 }
