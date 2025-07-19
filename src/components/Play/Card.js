6e3d2857d54da443bf19e4a6672ebdfe06e75a7f
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-07-19 00:07:16 -0400

    Create card component

--- src/components/Play/Card.js
+++ src/components/Play/Card.js
@@ -1,5 +1,10 @@
+import styles from "./Card.module.css"
 
-
-export default function Cards() {
+export default function Cards(props) {
+    return (
+        <div className={styles.cardsContainer}>
+            <h1>{props.value}</h1>
+        </div>
+    )
 
 }
